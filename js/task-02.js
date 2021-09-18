6dcef106c2e82ee9a54c59a8d9e372cf1939b213
Maryna Shevchenko <Shellma2011@gmail.com>
2021-09-19 01:35:12 +0300

    task-02.js-done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+ingredients.forEach(ingredient => {
+   const list = document.createElement('li');
+   listEl.appendChild(list);
+   list.textContent = ingredient;
+});
+
+// console.log(ingredients)
