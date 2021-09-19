ef895757c3216c42f0e06244558f32f65b69d01d
Maryna Shevchenko <Shellma2011@gmail.com>
2021-09-20 01:02:33 +0300

    task-06.js-done

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,17 @@
-const validationInput = document.querySelector('#validation-input');
-const inputLength = validationInput.dataset.length;
+const validationInputEl = document.querySelector('#validation-input');
 
-const input = document.querySelector('input');
+validationInputEl.addEventListener('blur', onValidationInputLength);
 
-validationInput.addEventListener('blur', element => {
-    const text = element.currentTarget.value;
-    
-    if (text.length == inputLength) {
-        validationInput.classList.add('valid');
-        validationInput.classList.remove('invalid');
-    } else {
-    validationInput.classList.add('invalid');
-    validationInput.classList.remove('valid');
-};
-})
+function onValidationInputLength(event) {
+    if (event.currentTarget.value.length == validationInputEl.dataset.length) {
+        validationInputEl.classList.remove('invalid');
+        validationInputEl.classList.add('valid');
+    }
+    else {
+        validationInputEl.classList.remove('valid');
+        validationInputEl.classList.add('invalid');
+    }
+}
 
 
 
